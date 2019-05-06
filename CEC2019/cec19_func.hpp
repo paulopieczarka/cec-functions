e4a8e681c6d19106bf53ca6b7f9ec91cad54b2fb
paulopieczarka <paulopieczarka@gmail.com>
2019-05-06 17:11:40 -0300

    Code lint & add OO

---
+++ CEC2019/cec19_func.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <iostream>
+#include <cstdlib>
+#include <math.h>
+
+#define INF 1.0e99
+#define EPS 1.0e-14
+#define E  2.7182818284590452353602874713526625
+#define PI 3.1415926535897932384626433832795029
+
+class Cec19 {
+  public:
+    Cec19();
+    ~Cec19();
+
+    void Lennard_Jones_func(double *, int, double *); /* Lennard Jones */
+    void Hilbert_func(double *, int, double *); /* Hilbert */
+    void Chebyshev_func(double *, int, double *); /* Chebyshev */
+    void Schaffer_F7_func(double *, double *, int, double *, double *, int, int); /* Schwefel's F7 */
+    void Ackley_func(double *, double *, int, double *, double *, int, int); /* Ackley's */
+    void Rastrigin_func(double *, double *, int, double *, double *, int, int); /* Rastrigin's */
+    void Step_rastrigin_func (double *, double *, int, double *, double *, int, int); /* Noncontinuous Rastrigin's */
+    void Weierstrass_func(double *, double *, int, double *, double *, int, int); /* Weierstrass's */
+    void Schwefel_func(double *, double *, int, double *, double *, int, int); /* Schwefel's */
+    void EScaffer6_func(double *, double *, int, double *, double *, int, int); /* Expanded Scaffer's F6 */
+    void Happycat_func(double *, double *, int, double *, double *, int, int); /* HappyCat */
+    void Griewank_func(double *, double *, int, double *, double *, int, int); /* Griewank's */
+
+    void test_func(double *, double *, int, int, int);
+
+  private:
+    double *OShift;
+    double *M;
+    double *y;
+    double *z;
+    double *x_bound;
+
+    int ini_flag;
+    int n_flag;
+    int func_flag;
+    int *SS;
+
+    void shift_func(double*, double*, int, double*);
+    void rotate_func(double*, double*, int, double*);
+    void sr_func(double *, double *, int, double*, double*, double, int, int); /* shift and rotate */
+    void asy_func(double *, double *x, int, double);
+    void osz_func(double *, double *, int);
+};
--- CEC2019/main.cpp
+++ CEC2019/main.cpp
@@ -4,74 +4,56 @@
   Sep. 21th 2018
 */
 
-// #include <WINDOWS.H>    
+#include <iomanip>
 #include <stdio.h>
-#include <math.h>
 #include <malloc.h>
+#include "cec19_func.hpp"
 
+int main (int argc, char *argv[]) {
+	Cec19 cec19 = Cec19();
 
-void cec17_test_func(double *, double *,int,int,int);
+	double *f, *x;
+	char FileName[48];
+	int i, j, k, n, m, func_num;
 
-double *OShift,*M,*y,*z,*x_bound;
-int ini_flag=0,n_flag,func_flag,*SS;
-
-
-void main()
-{
-	int i,j,k,n,m,func_num;
-	double *f,*x;
 	FILE *fpt;
-	char FileName[10];
-	m=2;
-	n=10;
-	x=(double *)malloc(m*n*sizeof(double));
-	f=(double *)malloc(sizeof(double)  *  m);
-	for (i = 0; i < 10; i++)
-	{
-		func_num=i+1;
-		sprintf(FileName, "input_data/shift_data_%d.txt", func_num);
-		fpt = fopen(FileName,"r");
-		if (fpt==NULL)
-		{
-			printf("\n Error: Cannot open input file for reading \n");
-		}
-		
-		if (x==NULL)
-			printf("\nError: there is insufficient memory available!\n");
+	m = 2;
+	n = 10;
+	x = (double *)malloc(m * n *sizeof(double));
+	f = (double *)malloc(sizeof(double)  *  m);
 
-		for(k=0;k<n;k++)
-		{
-				fscanf(fpt,"%lf",&x[k]);
-				/*printf("%lf\n",x[k]);*/
+	for (i = 0; i < 10; i++) {
+		func_num = i + 1;
+		sprintf(FileName, "input_data/shift_data_%d.txt", func_num);
+
+		fpt = fopen(FileName, "r");
+		if (fpt == NULL) {
+			std::cout << "Error: Cannot open input file for reading" << std::endl;
+		}
+
+		if (x == NULL) {
+			std::cout << "Error: there is insufficient memory available" << std::endl;
+		}
+
+		for (k = 0; k < n; k++) {
+			fscanf(fpt, "%lf", &x[k]);
 		}
 
 		fclose(fpt);
 
-			for (j = 0; j < n; j++)
-			{
-				x[1*n+j]=0.0;
-				/*printf("%lf\n",x[1*n+j]);*/
-			}
-		
-		
-		for (k = 0; k < 1; k++)
-		{
-			cec19_test_func(x, f, n,m,func_num);
-			for (j = 0; j < 2; j++)
-			{
-				printf(" f%d(x[%d]) = %lf,",func_num,j+1,f[j]);
-			}
-			printf("\n");
+		for (j = 0; j < n; j++) {
+			x[1 * n + j] = 0.0;
 		}
-	
+
+		for (k = 0; k < 1; k++) {
+			cec19.test_func(x, f, n, m, func_num);
+			for (j = 0; j < 2; j++) {
+				std::cout << "-> f" << func_num << "(x[" << (j + 1) << "]) = " << std::setprecision(16) << f[j] << std::endl;
+			}
+
+			std::cout << std::endl;
+		}
 	}
-	free(x);
-	free(f);
-	free(y);
-	free(z);
-	free(M);
-	free(OShift);
-	free(x_bound);
+
+	return 1;
 }
-
-
